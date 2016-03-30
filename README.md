03f5d52ccac2c5da1de624d765da84d2654472eb
cferko <cferko@domeyard.com>
2016-03-30 06:45:57 -0400

    adding setup.py

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 This is the student-facing signup repo for the Eldritch edularp.
+
+[![Binder](http://mybinder.org/badge.svg)](http://mybinder.org/repo/cferko/eldritch-signup)
---
+++ setup.py
@@ -0,0 +1,80 @@
+import subprocess
+from IPython.core.magic import (Magics, magics_class, line_magic,
+                                cell_magic, line_cell_magic)
+import os         
+from datetime import datetime
+import pandas as pd
+
+class Character():
+    def __init__(self):
+        self.character_name = None
+        self.real_name = None
+        self.email = None
+        self.race = None
+        self.house = None
+        
+def get_info(house):
+    pass
+
+@magics_class
+class MyMagics(Magics):
+
+    @line_magic
+    def checkpoint(self, line):
+        """For signup, just save name if it exists
+        """
+        try:
+            if (me.character_name != None and
+                me.real_name != None and
+                me.email != None and
+                me.race != None and
+                me.house != None):
+                
+                commit(me)
+                
+                print "Checkpoint complete. You have been signed up for Eldritch."
+                
+            else:
+                print "There was a problem with one of your inputs."
+        
+        except:
+            print "there was a problem"
+
+def commit(char):
+    subprocess.call(["cd /home/main/", 
+                     "git clone https://github.com/cferko/eldritch-signup.git",
+                     "cd eldritch-signup",
+                     "mkdir records",
+                     'git config --global user.email "ferko7@hotmail.com"',
+                     'git config --global user.name   "jttalks"',
+                     'git pull'], shell=True)
+    my_indices = [int(f) for f in os.listdir('~/main/')]
+    new_index = str(max(my_indices)+1)
+    
+    timestamp = datetime.now().ctime()
+    
+    my_data = pd.Series([me.real_name,
+                         me.character_name,
+                         me.email,
+                         me.race,
+                         me.house,
+                         timestamp])
+    
+    my_data.to_csv("/home/main/eldritch-signup/records/"+new_index,
+                   index=False)
+                   
+    subprocess.call(["cd /home/main/eldritch-signup/records/",
+                     "git add "+new_index,
+                     'git commit -m "ADD: signup #'+new_index+'"',
+                     'git push -u origin master',
+                     'jttalks',
+                     'jttalks1'], shell=True)
+    
+
+if __name__ == "__main__":
+    ip = get_ipython()
+    ip.register_magics(MyMagics)
+    
+    me = Character()
+    
+    print "Eldritch setup complete."
