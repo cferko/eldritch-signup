6c60782d9534274985172ec20f47614b8791ac6c
cferko <cferko@domeyard.com>
2016-04-16 16:49:37 -0400

    REV: Fixing random_name and get_info

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 import subprocess
+import pexpect
 from IPython.core.magic import (Magics, magics_class, line_magic,
                                 cell_magic, line_cell_magic)
 import os         
 from datetime import datetime
-import pandas as pd
-import pexpect
+import pandas as pd, numpy as np
+
 
 class Character():
     def __init__(self):
@@ -13,9 +14,6 @@ class Character():
         self.email = None
         self.race = None
         self.house = None
-        
-def get_info(house):
-    pass
 
 @magics_class
 class MyMagics(Magics):
@@ -71,18 +69,18 @@ def commit():
                events={'Username*':'jttalks\n', 'Password*':'jttalks1\n'})    
 
 house_dict = {
-    'cadon': 'Bearers of the Mark of Making, the artificers of House Cadon are responsible for creating the Forgelings, Zephyr, and the airships.',
-    'donric': 'House Donric is made up of soldiers and bodyguards with the Mark of Sentinel, and its Blade Guild is the only standing army in Caeros.',
-    'ghammara': 'Those with the Mark of Hospitality license inns and restaurants throughout Caeros and operate enclaves that offer sanctuary to fugitives and refugees.',
-    'jeordo': 'The medics of House Jeordo use the Mark of Healing to mend bones and cure diseases -- for those that can afford it.',
-    'kjaldar':'The banker of choice for the wealthy and powerful, House Kjaldar uses the Mark of Warding to guard strongholds and vaults containing great wealth.',
-    'larenthil':'The Larenthils are masters of sea and sky, using the Mark of Storm to control the weather and operate elemental airships.',
-    'micaeli':'Nothing escapes the notice of House Micaeli, whose members bear the Mark of Detection and are master investigators, researchers, and spy catchers.',
-    'oraite':'Oraite carries the Mark of Passage and dominate the business of travel, operating the massive magical train called Zephyr which travels across Caeros.',
-    'phaelanmyr':'Those of House Phaelanmyr bear the Mark of Shadow, and carry two faces: they are the house of entertainment, music, and art, and the house of spies and secrecy.',
-    'saryn':'Bearing the Mark of Scribing, the Saryns are masters of the written word, working as mediators, translators, and mediators of the law.',
-    'thagash':'The youngest house, Thagash consists of reckless prospectors and bounty hunters who use the Mark of Finding to locate deposits of Eldershards and dangerous criminals alike.',
-    'vernalis':'Those of House Vernalis bear the Mark of Handling, which gives them a bond to natural creatures; their main business is breeding magical and ordinary animals.'
+    'cadon': 'Bearers of the Mark of Making, the artificers of House Cadon are responsible for creating the Forgelings, Zephyr, and the airships.\n',
+    'donric': 'House Donric is made up of soldiers and bodyguards with the Mark of Sentinel, and its Blade Guild is the only standing army in Caeros.\n',
+    'ghammara': 'Those with the Mark of Hospitality license inns and restaurants throughout Caeros and operate enclaves that offer sanctuary to fugitives and refugees.\n',
+    'jeordo': 'The medics of House Jeordo use the Mark of Healing to mend bones and cure diseases -- for those that can afford it.\n',
+    'kjaldar':'The banker of choice for the wealthy and powerful, House Kjaldar uses the Mark of Warding to guard strongholds and vaults containing great wealth.\n',
+    'larenthil':'The Larenthils are masters of sea and sky, using the Mark of Storm to control the weather and operate elemental airships.\n',
+    'micaeli':'Nothing escapes the notice of House Micaeli, whose members bear the Mark of Detection and are master investigators, researchers, and spy catchers.\n',
+    'oraite':'Oraite carries the Mark of Passage and dominate the business of travel, operating the massive magical train called Zephyr which travels across Caeros.\n',
+    'phaelanmyr':'Those of House Phaelanmyr bear the Mark of Shadow, and carry two faces: they are the house of entertainment, music, and art, and the house of spies and secrecy.\n',
+    'saryn':'Bearing the Mark of Scribing, the Saryns are masters of the written word, working as mediators, translators, and mediators of the law.\n',
+    'thagash':'The youngest house, Thagash consists of reckless prospectors and bounty hunters who use the Mark of Finding to locate deposits of Eldershards and dangerous criminals alike.\n',
+    'vernalis':'Those of House Vernalis bear the Mark of Handling, which gives them a bond to natural creatures; their main business is breeding magical and ordinary animals.\n'
     }
     
 marked_houses = ['Cadon',
@@ -104,10 +102,44 @@ def get_info(house):
     except:
         print "The house you entered was not recognized."
         return
+
+def random_name(race, gender):
+    global name_frame    
+    my_frame = name_frame.copy()    
+    
+    try:
+        if race.lower()=='nantangil':
+            this_race = 'Nantangil'
+        elif race.lower()=='human':
+            this_race = 'Human'
+        elif race.lower()=='forgeling':
+            this_race = 'Forgeling'
+    except:
+        this_race = None
         
+    try:
+        if gender.lower()=="male":
+            this_gender="Male"
+        elif gender.lower()=='female':
+            this_gender = 'Female'
+            
+    except:
+        this_gender = None        
+        
+    if this_race != None:
+        my_frame = my_frame[my_frame["Race"]==this_race]
+    if this_gender !=None:
+        my_frame = my_frame[my_frame["Gender"]==this_gender]
+        
+    names = my_frame["Name"].values
+    
+    return np.random.choice(names)
+
 if __name__ == "__main__":
     ip = get_ipython()
     ip.register_magics(MyMagics)
+    
+    name_frame = pd.read_csv("names_df.csv")  
     
     me = Character()
     
